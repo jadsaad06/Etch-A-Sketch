6bfb98778ae5f7b591b0a61d1da888a27549f6fa
Jad Saad <jadsaad06@gmail.com>
2024-09-16 01:25:59 -0700

    got the grid working using flex-basis and a calculation

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const gridContainer = document.querySelector("#grid-container");
 for(let i = 0; i < 256; i++){
     const div = document.createElement("div");
     div.classList.add("grid-box");
+
+    // div.addEventListener("mouseenter", function(){
+        div.style.backgroundColor = "black";
+    //})
+
+
     gridContainer.appendChild(div);
 }
