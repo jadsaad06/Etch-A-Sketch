1b1efba394cd1b52355eea5c0db7ebd4f5cf98b5
Jad Saad <jadsaad06@gmail.com>
2024-09-16 00:35:20 -0700

    added container id

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gridContainer = document.querySelector("grid-container");
+const gridContainer = document.querySelector("#grid-container");
 
 for(let i = 0; i < 256; i++){
     const div = document.createElement("div");
