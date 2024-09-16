6bbdf7e8b9b1e83436f61341bcef0f5d157b3999
Jad Saad <jadsaad06@gmail.com>
2024-09-16 02:00:00 -0700

    implemented reset button

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
+//set initial grid size
+let gridSize = 16;
+
+//get grid container
 const gridContainer = document.querySelector("#grid-container");
 
-for(let i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    div.classList.add("grid-box");
-    div.style.backgroundColor = "grey";
+//set grid to grid size
+let setGrid = function(gridSize){
+    for(let i = 0; i < gridSize*gridSize; i++){
+        const div = document.createElement("div");
+        div.classList.add("grid-box");
     
-     div.addEventListener("mouseenter", function(){
-        div.style.backgroundColor = "black";
-    })
-
-
-    gridContainer.appendChild(div);
+        div.setAttribute("style", `flex: 0 0 calc(100%/${gridSize}); background-color: grey`);
+    
+         div.addEventListener("mouseenter", function(){
+            div.style.backgroundColor = "black";
+        })
+    
+        gridContainer.appendChild(div);
+    }
 }
+
+//create reset button
+const reset = document.querySelector("#reset");
+
+reset.addEventListener("click", function(){
+    const div = document.querySelectorAll(".grid-box");
+
+    div.forEach((gridBox) => {
+        gridBox.style.backgroundColor = "grey";
+    });
+})
+
+
+
+//start the program
+setGrid(gridSize);
